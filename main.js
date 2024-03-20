330643549f4c623aaee8dd261a0cbfb85baa0203
Saba Nizam <160767347+SabaNizam90@users.noreply.github.com>
2024-03-20 21:54:24 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var A2 = "Saba Nizam";
+console.log(A2);
+var X = "Typescript variables";
+console.log(X);
+console.log(X);
+console.log(X);
+console.log(X);
+console.log(X);
+console.log(X);
---
+++ main.ts
@@ -0,0 +1,10 @@
+let hi = "Saba Nizam"
+console.log(hi);
+
+let X = "Typescript variables"
+console.log(X);
+console.log(X);
+console.log(X);
+console.log(X);
+console.log(X);
+console.log(X);
